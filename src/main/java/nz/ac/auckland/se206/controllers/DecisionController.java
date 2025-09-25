d6da0b152b03b7cb9e295114af766da02abefaeb
Dandan Wu <jwu539@aucklanduni.ac.nz>
2025-09-26 02:21:18 +1200

    changed default fallback to incorrect for situations when player submit is empty

--- src/main/java/nz/ac/auckland/se206/controllers/DecisionController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/DecisionController.java
@@ -30,7 +30,7 @@ public class DecisionController implements Initializable {
       } else if ("Not Guilty".equals(userChoice)) {
         result.setText("You are INCORRECT");
       } else {
-        result.setText("You are CORRECT"); // Default fallback
+        result.setText("You are INCORRECT"); // Default fallback, if player submit no decision
       }
     }
 
