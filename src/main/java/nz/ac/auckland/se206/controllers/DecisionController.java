a10b61cd808a9dfd0af7a543a8dcbd21c4f0376e
Harsahilbasra <hbas514@aucklanduni.ac.nz>
2025-09-25 17:45:48 +1200

    change frontend of result so can accomodate being dynamic

--- src/main/java/nz/ac/auckland/se206/controllers/DecisionController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/DecisionController.java
@@ -2,11 +2,16 @@ package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameSession;
 import nz.ac.auckland.se206.GameStateContext;
 
 public class DecisionController {
+
+  @FXML private Label result;
+  @FXML private Label gptFeedback;
+
   @FXML
   public void onReturnClicked() throws IOException {
     GameSession s = GameStateContext.getSession();
