0259c7c6e8e2db0e5c9163de2828cf6f10c5f859
Dandan Wu <jwu539@aucklanduni.ac.nz>
2025-10-17 17:20:44 +1300

    binded controller to fxml, implemented logic for slider changing image

--- src/main/java/nz/ac/auckland/se206/controllers/SecurityCameraController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/SecurityCameraController.java
@@ -1,7 +1,10 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 
@@ -13,4 +16,27 @@ public class SecurityCameraController {
   @FXML private ImageView camera;
   @FXML private BorderPane backgBorderPane;
   @FXML private Slider cameraSlider;
+
+  /** Initializes the security camera view. */
+  @FXML
+  private void initialize() {
+    // Set initial values for the camera view and slider
+    camera.setImage(new Image("/images/Security_NoAI.png"));
+    cameraSlider.setValue(0);
+    cameraSlider
+        .valueProperty()
+        .addListener(
+            new ChangeListener<Number>() {
+              @Override
+              public void changed(
+                  ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                // Checking if slider is between certain ranges to change the image accordingly
+                if (newValue.doubleValue() >= 55 && newValue.doubleValue() <= 60) {
+                  camera.setImage(new Image("/images/Security_WithAI.png"));
+                } else {
+                  camera.setImage(new Image("/images/Security_NoAI.png"));
+                }
+              }
+            });
+  }
 }
